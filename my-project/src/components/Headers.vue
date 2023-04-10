16a22c0f12db12a345e8e2329238e65f48d7e7d0
Mohammed <johndoe@email.com>
2023-04-10 22:24:57 +0300

    ؤاشىلث

--- my-project/src/components/Headers.vue
+++ my-project/src/components/Headers.vue
@@ -166,7 +166,7 @@ export default {
         <div class="mt-6 flow-root">
           <div class="-my-6 divide-y divide-gray-500/10">
             <div class="space-y-2 py-6">
-              <router-link to="/" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+              <router-link to="/Services" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
           >Services</router-link
         >
 
