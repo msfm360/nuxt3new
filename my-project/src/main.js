f2d3f21444b09069c9a0cef626ee7008a441cc8a
Mohammed <johndoe@email.com>
2023-04-19 02:07:58 +0300

    usehead

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -4,10 +4,14 @@ import App from './App.vue'
 import { MotionPlugin } from '@vueuse/motion'
 import store from './store'
 import router from './router'
+import { createHead } from "@vueuse/head"
+
 // x
 
 
 const app = createApp(App)
+const head = createHead()
+app.use(head)
 app.use(store)
 app.use(MotionPlugin)
 app.use(router)
