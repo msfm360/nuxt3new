e84ec68b73828dba32058a22ce47a0379bce81c0
Mohammed <johndoe@email.com>
2023-04-19 02:43:22 +0300

    sitemap

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
-// import generateSitemap from "vite-plugin-pages-sitemap";
+import generateSitemap from "vite-plugin-pages-sitemap";
+import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // Pages({
-    //   onRoutesGenerated: (routes) => generateSitemap({ routes }),
-    // }),
+    Pages({
+      onRoutesGenerated: async (routes) => {
+        const names = await getNames();
+        const dynamicRoutes = names.map((name) => `/names/${name}`);
+        generateSitemap({
+          hostname: "https://estehdath.com/",
+          routes: [...routes, ...dynamicRoutes],
+          readable: true,
+        });
+      },
+    }),
     vue(),
     VitePWA({ registerType: "autoUpdate" }),
   ],
