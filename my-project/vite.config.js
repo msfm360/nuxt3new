7bba25d7b4ee8f12bb8ab222f387a47cbc5eb17c
Mohammed <johndoe@email.com>
2023-04-19 02:15:32 +0300

    sitemap

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
+import generateSitemap from "vite-plugin-pages-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),    VitePWA({ registerType: 'autoUpdate' })
-],
-  
-})
+  plugins: [
+    Pages({
+      onRoutesGenerated: (routes) => generateSitemap({ routes }),
+    }),
+    vue(),
+    VitePWA({ registerType: "autoUpdate" }),
+  ],
+});
