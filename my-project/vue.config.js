acb8d211688050152dc1553587917c1432b0b857
Mohammed <johndoe@email.com>
2023-04-19 04:27:30 +0300

    sitemap

--- my-project/vue.config.js
+++ my-project/vue.config.js
@@ -1,9 +1,19 @@
 const { GenerateSW } = require("workbox-webpack-plugin");
+require = require('esm')(module);
+const { routes } = require('./src/routes.js');
+
 
 module.exports = {
   publicPath: process.env.NODE_ENV === "development" ? "/vuejs-pwa/" : "",
 
   configureWebpack: {
     plugins: [new GenerateSW()]
-  }
+  },
+  pluginOptions: {
+		sitemap: {
+			baseURL: 'https://estehdath.com',
+			routes,
+		}
+	}
+
 };
