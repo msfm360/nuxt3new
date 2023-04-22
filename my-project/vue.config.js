b3654992aa3725b4ff75eeccf93cc87ee0775b3e
Mohammed <johndoe@email.com>
2023-04-22 16:15:26 +0300

    sitemapxml webpack plugin

--- my-project/vue.config.js
+++ my-project/vue.config.js
@@ -1,19 +1,30 @@
 const { GenerateSW } = require("workbox-webpack-plugin");
 require = require('esm')(module);
 const { routes } = require('./src/routes.js');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+const paths = [
+  {
+      path: '/',
+      lastmod: new Date().toISOString().slice(0,10),
+      priority: '0.8',
+      changefreq: 'hourly'
+  },
+];
 
 
 module.exports = {
   publicPath: process.env.NODE_ENV === "development" ? "/vuejs-pwa/" : "",
 
   configureWebpack: {
-    plugins: [new GenerateSW()]
-  },
-  pluginOptions: {
-		sitemap: {
-			baseURL: 'https://estehdath.com',
-			routes,
-		}
-	}
+    plugins: [
+        new SitemapPlugin('https://estehdath.com', paths, {
+            filename: 'sitemap.xml',
+            lastmod: true,
+            changefreq: 'hourly',
+            priority: '0.8'
+        })
+    ]
+}
+
 
 };
